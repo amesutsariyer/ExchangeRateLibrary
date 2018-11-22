fc846c70de33e0d94ffba86cf50dec818f97c82d
ahmet.sariyer <ahmet.sariyer@kafein.com.tr>
2018-11-22 13:53:25 +0300

    proje class Lib ve dosyalar oluşturuldu

---
+++ ExhangeRateService/Service/ExchangeRateService.cs
@@ -0,0 +1,19 @@
+﻿using ExhangeRateService.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExhangeRateService.Service
+{
+    
+    public class ExchangeRateService : IExchangeRateService
+    {
+        private readonly Service _service;
+        public ExchangeRateService()
+        {
+            _service = new Service();
+        }
+    }
+}
---
+++ ExhangeRateService/Service/Service.cs
@@ -0,0 +1,13 @@
+﻿using ExhangeRateService.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExhangeRateService.Service
+{
+    public class Service : IService
+    {
+    }
+}
